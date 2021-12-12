36150c2c92bd12ce53222fa44da6017ff6275edb
leekaimun <leekaimun@gmail.com>
2021-12-13 01:01:01 +0800

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Sign In With Apple'),
+      home: MyHomePage(title: 'Sign In'),
     );
   }
 }
